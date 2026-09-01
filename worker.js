9644d03f1814727f5ac0fa3144cc9cf99de85789
jokiatgithub <17127870+jokiatgithub@users.noreply.github.com>
2026-09-01 17:31:24 +0200

    feat: add consent-gated PostHog analytics

--- worker.js
+++ worker.js
@@ -1,5 +1,12 @@
+const analyticsAssets = `
+<link data-blinkin-analytics href="https://blinkin.de/cookieconsent/cookieconsent.css" rel="stylesheet">
+<link href="https://blinkin.de/cookie-consent.css" rel="stylesheet">
+<script defer src="https://blinkin.de/cookieconsent/cookieconsent.umd.js"></script>
+<script type="text/plain" data-category="analytics" data-service="PostHog" src="https://blinkin.de/posthog-init.js"></script>
+<script defer src="https://blinkin.de/cookie-consent.js"></script>`;
+
 export default {
-  fetch(request, env) {
+  async fetch(request, env) {
     const url = new URL(request.url);
 
     if (url.hostname === 'vorsprung.blinkin.io') {
@@ -16,9 +23,18 @@ export default {
 
     if (url.pathname === '/' || url.pathname === '/index.html') {
       url.pathname = '/v15/';
-      return env.ASSETS.fetch(new Request(url, request));
+      request = new Request(url, request);
     }
 
-    return env.ASSETS.fetch(request);
+    const response = await env.ASSETS.fetch(request);
+    if (!response.headers.get('content-type')?.includes('text/html')) return response;
+
+    return new HTMLRewriter()
+      .on('head', {
+        element(element) {
+          element.append(analyticsAssets, { html: true });
+        },
+      })
+      .transform(response);
   },
 };
